02257d6a1c87499df8425a06c863fef5163c858c
codekyd <okpusjamin@gmail.com>
2019-11-12 02:45:17 +0100

    fix webpack and babel error and complied successfully

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 
 
+const path = require('path');
+
 module.exports = {
-    target: "node",
+  entry: './src/server.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.bundle.js'
+  },
+  target: "node",
     externals: ['pg', 'sqlite3', 'tedious', 'pg-hstore'],
-  }
+};
